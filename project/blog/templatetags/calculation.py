2a6e07b9c44a544ac645bc2d2dd4c965e6c44a9f
windows10_huangbin <16274382@qq.com>
2019-02-12 22:24:08 +0800

    完善搜索结果页面

--- project/blog/templatetags/calculation.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Administrator'
-from django import template
-from django.utils.safestring import mark_safe
-register = template.Library()
-@register.simple_tag
-
-def getCouponNum(instance, coupon_info):
-    return coupon_info.split('减')[1].split('元')[0]
--- project/blog/views.py
+++ project/blog/views.py
@@ -12,11 +12,27 @@ def search(request):
     param = {
         'method':'taobao.tbk.dg.material.optional',
         'q':product_name,
-        'adzone_id':'91132500175'
+        'adzone_id':'91132500175',
+        'platform':2
     }
     req = TbkRequest.TbkDgMaterialOptionalRequest(param)
     res = req.getResponse()
-    data = {}
+    data = {
+        'q':product_name
+    }
     if res is not False:
-        data = {'q': product_name, 'res': res}
+        map_data = res['result_list']['map_data']
+        coupon = []
+        not_coupon = []
+        for values in map_data:
+            if len(values['coupon_id']) > 0:
+                values['coupon_share_url'] = values['coupon_share_url'].replace('\\','')
+                values['coupon_info'] = values['coupon_info'].split('减')[1]
+                coupon.append(values)
+            else:
+                not_coupon.append(values)
+        res = []
+        res.extend(coupon)
+        res.extend(not_coupon)
+        data['res'] = res
     return render(request, 'search.html', {'data' : data})
